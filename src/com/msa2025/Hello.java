d7db4d6e163bc5ea5fca7e56a789d8e252ee5846
djdjcoding <114716161+djdjcoding@users.noreply.github.com>
2025-03-14 16:39:40 +0900

    3.브라우저에서 수정함

--- src/com/msa2025/Hello.java
+++ src/com/msa2025/Hello.java
@@ -4,6 +4,6 @@ public class Hello {
 	public static void main(String[] args) {
 		System.out.println("안녕하세요");
 		System.out.println("안녕하세요");
-		System.out.println("브라우저에서 수정");
+		System.out.println("브라우저에서 수정2");
 	}
 }
